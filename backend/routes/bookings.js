082c04442a9c806e5422b6945e49605754dabbd2
Arjun Sreekumar <134309199+ArjuntheSreekumar@users.noreply.github.com>
2024-12-28 04:24:56 +0530

    Delete backend directory

--- backend/routes/bookings.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { createBooking ,getAllBooking,getBooking} from "../controllers/bookingController.js";
-const router=express.Router();
-router.post("/",createBooking);
-router.get("/:id",getBooking);
-router.post("/",getAllBooking);
-export default router;
--- backend/routes/reviews.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { createReview } from '../controllers/reviewController.js';
-
-const router = express.Router();
-router.post('/:tourId', createReview);
-export default router;
